e8013dad2c6be8dab6bc7a2e4b711266f16373d4
rikki-tamaki <ic041223@gmail.com>
2020-10-24 17:54:58 +0900

    parser

--- skill/human-response.js
+++ skill/human-response.js
@@ -30,6 +30,7 @@ module.exports = class SkillHumanResponse {
                     }
                 },
                 parser: (value, bot, event, context, resolve, reject) => {
+                    debug("hogehoge");
                     return parser.parse("yes_no", value, resolve, reject);
                 },
                 reaction: (error, value, bot, event, context, resolve, reject) => {
